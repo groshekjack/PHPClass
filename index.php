b0da5a55e3938a08e8951f9a2fdfcdac129d3232
Jack Groshek <700175337@fvtc.edu>
2018-02-07 17:54:49 -0600

    Message

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Class - Jack Groshek</title>
+    <link rel="stylesheet" type="text/css" href="css/mystyles.css"/>
+</head>
+
+<body>
+<div class="pagewrap">
+    <header class="site-header">
+        <h1>PHP Class - Jack Groshek</h1>
+    </header>
+    <div class="flex-box">
+    <nav class="main-nav">
+    <ul>
+        <li class="menu/item"><a href="/">Home</a></li>
+        <li class="menu/item"><a href="#">Loops</a></li>
+        <li class="menu/item"><a href="#">Strings</a></li>
+    </ul>
+    </nav>
+
+    <section class="main-content">
+        <div class="img">picture/div>
+        <h2>Hello!</h2>
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor urna, malesuada at laoreet id,
+                lacinia eget mauris. Nullam a arcu neque.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor urna, malesuada at laoreet id,
+                lacinia eget mauris. Nullam a arcu neque.</p>
+        </div>
+    </section>
+
+    <aside class="right-sidebar">Sidebar</aside>
+    </div> <!-- /flex box -->
+
+    <footer>
+        <div>Learn PHP <sup>&copy;</sup> 2018</div>
+    </footer>
+
+</div> <!-- /page wrap -->
+
+
+</body>
+<html>
